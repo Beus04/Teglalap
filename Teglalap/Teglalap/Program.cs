6aef9b71020558053a8701f02fa5d97ed603c9ab
Bea Portik-Cseres <portik.cseres.bea@gmail.com>
2022-11-04 21:57:53 +0100

    Init solution

---
+++ Teglalap/Teglalap/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Teglalap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Teglalap[] teglalapok = new Teglalap[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                teglalapok[i] = new Teglalap();
+            }
+
+            foreach (var item in teglalapok)
+            {
+                Console.WriteLine(item.ToString());
+                Console.WriteLine();
+            }
+
+             
+            
+            
+
+        }
+    }
+}
---
+++ Teglalap/Teglalap/Teglalap.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Teglalap
+{
+    public class Teglalap   
+    {
+        private int a = 0;
+        private int b = 0;
+
+        public Teglalap()
+        {
+            Random random = new Random();
+
+            a = random.Next(1, 100);
+            b = random.Next(1, 100);
+        }
+
+        public bool NegyzetE()
+        {
+            return a == b;       
+        }
+
+        public override string ToString()
+        {
+            string negyzete = NegyzetE() ? "Ez a téglalap egyben négyzet is." : "Ez a téglalap nem négyzet.";
+            return $"A téglalap szélessége: {a} {Environment.NewLine}A téglalap magassága: {b}{Environment.NewLine}{negyzete} "; 
+        }
+
+    }
+
+
+
+}
